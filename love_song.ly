8b1d3baaf607c38a6efea076dcf76aa17f977e6b
mirrorecho <info@randallwest.com>
2015-01-08 15:03:13 -0600

    lyrics work!

---
+++ love_song.ly
@@ -0,0 +1,620 @@
+lovelyrics = \lyricmode {                                   I               once                  lived like   a     mouse                   a-               lone  in    my    house                 when            fate                    in-  ter-               up-    ted    my    life                    fate                          swept me  a-     way                                             and               sud- den- ly one  day                  I               found                   my- self                    in                    charge   of   a  play                                                    it      be- came            my              miss -ion               to               save  a   tra-    di- tion I               must            strive         to      keep    the                    foll- ies    a- live                                            and  help            them            thrive                                      Now             --               all  of    these  throngs                 need- ed               all  of    these  songs                 and                           won- der- ful            speech-  es   to     speak                   fate                          swept me  a-     way                                             and               sud- den- ly one  day                  I               found                   my- self                    in                    charge   of   a  play                                      I               once                  lived like   a     mouse                   a-               lone  in    my    house         and     ne-     ver     thought I`d     be              an- y- one`s       wife                    } 
+
+% 2015-01-08 15:00
+
+\version "2.18.2"
+\language "english"
+
+#(set-global-staff-size 16)
+
+\header {
+	composer = \markup { Randall West }
+	title = \markup { An Undecided... }
+}
+
+\layout {
+	\context {
+		\override VerticalAxisGroup #'remove-first = ##t
+	}
+}
+
+\paper {
+	system-system-spacing = #'((basic-distance . 0) (minimum-distance . 0) (padding . 12) (stretchability . 0))
+}
+
+\score {
+	\new Score \with {
+		\override StaffGrouper #'staff-staff-spacing = #'((basic-distance . 0) (minimum-distance . 0) (padding . 6) (stretchability . 0))
+		\override VerticalAxisGroup #'staff-staff-spacing = #'((basic-distance . 0) (minimum-distance . 0) (padding . 6) (stretchability . 0))
+		markFormatter = #format-mark-box-numbers
+	} <<
+		\new Staff {
+       \new Voice ="voice" {
+			\set Staff.instrumentName = \markup { Voice }
+			\set Staff.shortInstrumentName = \markup { . }
+			{
+				R1
+				R1
+				R1
+				r2
+				f'4 \p
+				e'4
+				\times 2/3 {
+					f'4
+					e'4
+					g'4
+				}
+				g'4
+				r8
+				f'8
+				\times 2/3 {
+					e'4
+					g'4
+					g'4
+				}
+				g'4
+				r4
+				r4
+				f'4 ^ \<
+				c'4
+				r8
+				c'16
+				c'16
+				\times 2/3 {
+					g'4
+					g'4
+					g'4
+				}
+				g'4 \mf
+				r4
+			}
+			{
+				R1
+				r4
+				g'4 \mf
+				\times 2/3 {
+					bf'4
+					a'4
+					f'4
+				}
+				f'2
+				r4
+				g'4
+				\times 2/3 {
+					bf'8
+					a'8
+					a'4
+					f'4
+				}
+				f'2
+				r4
+				f'4
+				g'4.
+				f'8
+				f'4
+				r8
+				a'8
+				\times 2/3 {
+					a'4
+					g'4
+					f'4
+				}
+				f'2.
+				r4
+				R1
+			}
+			{
+				R1
+				r2
+				r4
+				f'8 \p
+				e'8
+				f'4
+				g'4
+				g'16
+				g'8.
+				r8
+				f'8
+				\times 2/3 {
+					e'4
+					g'4
+					g'4
+				}
+				g'16
+				g'8.
+				r4
+				f'4 \<
+				e'4
+				c''2 \f
+				r8
+				g'8
+				g'8 [
+				g'8 ]
+				\times 2/3 {
+					bf'4
+					a'4
+					f'4
+				}
+				f'2
+				r4
+				g'4
+				g'4
+				g'4
+				a'2
+			}
+			{
+				R1
+				R1
+				R1
+				r2
+				f'4 \p (
+				e'4 )
+				\times 2/3 {
+					f'4
+					e'4
+					g'4
+				}
+				g'4
+				r8
+				f'16
+				e'16
+				\times 2/3 {
+					e'4
+					g'4
+					g'4
+				}
+				g'4
+				r4
+				r4
+				f'4 ^ \<
+				\times 2/3 {
+					c'4
+					c'4
+					c'4
+				}
+				\times 2/3 {
+					g'4
+					g'4
+					g'4
+				}
+				g'4 \mf
+				r4
+			}
+			{
+				R1
+				r4
+				g'4 \mf
+				\times 2/3 {
+					bf'4
+					a'4
+					f'4
+				}
+				f'2
+				r4
+				g'4
+				\times 2/3 {
+					bf'8
+					a'8
+					a'4
+					f'4
+				}
+				f'2
+				r4
+				f'4
+				g'4.
+				f'8
+				f'4
+				r8
+				a'8
+				\times 2/3 {
+					a'4
+					g'4
+					f'4
+				}
+				f'2.
+				r4
+				R1
+			}
+			{
+				R1
+				R1
+				R1
+				r2
+				f'4 \p
+				e'4
+				\times 2/3 {
+					f'4
+					e'4
+					g'4
+				}
+				g'4
+				r8
+				f'8
+				\times 2/3 {
+					e'4
+					g'4
+					g'4
+				}
+				g'4
+				r4
+				r8
+				f'8 ^ \<
+				f'8
+				f'8
+				c'8
+				c'8
+				c'4
+				\times 2/3 {
+					g'4
+					g'4
+					g'4
+				}
+				g'4 \mf
+				r4
+			}
+			{
+				R1
+				R1
+				R1
+				R1
+				R1
+				R1
+				R1
+				R1
+			}
+			{
+				R1
+				R1
+				R1
+				R1
+				R1
+				R1
+				R1
+				R1
+			}
+		}
+		}
+    \new Lyrics \lyricsto voice \lovelyrics 
+    \new PianoStaff <<
+			\set PianoStaff.instrumentName = \markup { Piano }
+			\set PianoStaff.shortInstrumentName = \markup { . }
+			\new Staff {
+				{
+					\times 2/3 {
+						d'4
+						<a' c''>4
+						e'4
+					}
+					<g' e''>4.
+					c''8
+					\times 2/3 {
+						d''4
+						<a'' c'''>4
+						e''4
+					}
+					<g'' e'''>2
+					f'8 [ (
+					d''8 ]
+					e''8 [
+					c''8 ] )
+					f''4 (
+					e''4
+					c'''2 )
+					f''8 [ (
+					d'''8 ]
+					e'''8 [
+					c'''8 ]
+					\times 2/3 {
+						f''4
+						<c''' e'''>4
+						g''4
+					}
+					<b'' g'''>4. )
+					f''8 (
+					\times 2/3 {
+						e''2
+						g''4
+					}
+					<b'' g'''>2 )
+					<c'' d'' f''>2
+					c''8.
+					e''16
+					c''8
+					c''16
+					c''16
+					R1
+				}
+				{
+					R1
+					r2
+					\times 2/3 {
+						<c' f' bf'>4
+						<c' f' a'>4
+						<c' f'>4
+					}
+					<c' d' f'>1
+					\times 2/3 {
+						<c' f' bf'>8
+						<c' f' a'>8
+						<c' f' a'>4
+						<c' f'>4
+					}
+					<c' d' f'>2
+					R1
+					r2
+					\times 2/3 {
+						<c' f' a'>4
+						g4
+						<c' f'>4
+					}
+					<c' d' f'>1
+					R1
+				}
+				{
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+				{
+					\times 2/3 {
+						d'4
+						<a' c''>4
+						e'4
+					}
+					<g' e''>4.
+					c''8
+					\times 2/3 {
+						d''4
+						<a'' c'''>4
+						e''4
+					}
+					<g'' e'''>2
+					f'8 [ (
+					d''8 ]
+					e''8 [
+					c''8 ] )
+					f''4 (
+					e''4
+					c'''2 )
+					f''8 [ (
+					d'''8 ]
+					e'''8 [
+					c'''8 ]
+					\times 2/3 {
+						f''4
+						<c''' e'''>4
+						g''4
+					}
+					<b'' g'''>4. )
+					f''8 (
+					\times 2/3 {
+						e''2
+						g''4
+					}
+					<b'' g'''>2 )
+					R1
+					R1
+				}
+				{
+					R1
+					r2
+					\times 2/3 {
+						<c' f' bf'>4
+						<c' f' a'>4
+						<c' f'>4
+					}
+					<c' d' f'>1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+				{
+					\times 2/3 {
+						d'4
+						<a' c''>4
+						e'4
+					}
+					<g' e''>4.
+					c''8
+					\times 2/3 {
+						d''4
+						<a'' c'''>4
+						e''4
+					}
+					<g'' e'''>2
+					f'8 [ (
+					d''8 ]
+					e''8 [
+					c''8 ] )
+					f''4 (
+					e''4
+					c'''2 )
+					f''8 [ (
+					d'''8 ]
+					e'''8 [
+					c'''8 ]
+					\times 2/3 {
+						f''4
+						<c''' e'''>4
+						g''4
+					}
+					<b'' g'''>4. )
+					f''8 (
+					\times 2/3 {
+						e''2
+						g''4
+					}
+					<b'' g'''>2 )
+					R1
+					R1
+				}
+				{
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+				{
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+			}
+			\new Staff {
+				\clef "bass"
+				{
+					a,1
+					f4
+					g4
+					a2
+					d'4
+					c'4
+					a2
+					a2
+					d'4 (
+					<a c'>4 ~
+					<a c'>4
+					<g e'>4 ~
+					<g e'>2 )
+					\times 2/3 {
+						<c a>4 (
+						g2 ~
+					}
+					g4.. )
+					c16
+					c4
+					c4
+					a,4
+					bf,4
+					c2
+					e,2
+				}
+				{
+					e,1
+					e,4
+					f,2.
+					R1
+					R1
+					r4
+					a,4
+					bf,2
+					bf,2
+					<a, f>2
+					d1
+					R1
+				}
+				{
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+				{
+					a,1
+					f4
+					g4
+					a2
+					d'4
+					c'4
+					a2
+					a2
+					d'4 (
+					<a c'>4 ~
+					<a c'>4
+					<g e'>4 ~
+					<g e'>2 )
+					\times 2/3 {
+						<c a>4 (
+						g2 ~
+					}
+					g4.. )
+					c16
+					c4
+					c4
+					a,4
+					bf,4
+					c2
+					e,2
+				}
+				{
+					e,1
+					e,4
+					f,2.
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+				{
+					a,1
+					f4
+					g4
+					a2
+					d'4
+					c'4
+					a2
+					a2
+					d'4 (
+					<a c'>4 ~
+					<a c'>4
+					<g e'>4 ~
+					<g e'>2 )
+					\times 2/3 {
+						<c a>4 (
+						g2 ~
+					}
+					g4.. )
+					c16
+					c4
+					c4
+					a,4
+					bf,4
+					c2
+					e,2
+				}
+				{
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+				{
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+					R1
+				}
+			}
+		>>
+	>>
+}
--- love_song.py
+++ love_song.py
@@ -1,6 +1,7 @@
 from abjad import *
 
 import copy
+import subprocess
 
 # any way to avoid this sys path part??
 import sys
@@ -9,16 +10,16 @@ sys.path.append("/Users/randallwest/Code/mirrorecho")
 from calliope.work import Arrangement, Project, Part
 
 # TO DO
-# - figure out lyrics
+# - (DONE... although pretty hacky) figure out lyrics
 # --- (assume will need to save lilypond strings into files from abjad, then add lyrics manually through lilypond)
-# - put arrangement on normal size paper
+# - (DONE) put arrangement on normal size paper
 
 class LoveArrangement(Arrangement):
     def __init__(self):
 
         super().__init__(name="love_song", title="An Undecided...", project=Project(name="rwestmusic-follies"))
-        self.add_part(name='voice', instrument=instrumenttools.MezzoSopranoVoice(instrument_name="Voice", short_instrument_name=""))
-        self.add_piano_staff_part(name='piano', instrument=instrumenttools.Piano(instrument_name="Piano", short_instrument_name=""))
+        self.add_part(name='voice', instrument=instrumenttools.MezzoSopranoVoice(instrument_name="Voice", short_instrument_name="."))
+        self.add_piano_staff_part(name='piano', instrument=instrumenttools.Piano(instrument_name="Piano", short_instrument_name="."))
 
 
 love_arrangement = LoveArrangement()
@@ -70,15 +71,18 @@ blank_lines = [
         ],
     ]
 
+lyrics = ""
+
 def extend_lines(lines):
     big_lines = ["", "", "", ""]
+    global lyrics
     for l in lines:
         big_lines[0] = big_lines[0] + l[0] + " " 
         big_lines[1] = big_lines[1] + l[1] + " " 
         big_lines[2] = big_lines[2] + l[2] + " " 
         big_lines[3] = big_lines[3] + l[3] + " " 
     love_arrangement.parts["voice"].extend("\\relative c' {" + big_lines[0] + "}")
-    # TO DO... figure out how to add voice part here...
+    lyrics += big_lines[1] + " "
     love_arrangement.parts["piano"][0].extend("\\relative c' {" + big_lines[2] + "}")
     love_arrangement.parts["piano"][1].extend("\\relative c {" + big_lines[3] + "}")
 
@@ -139,23 +143,23 @@ mouse_lines = [
            #0 ||-------|------||-------|------||-------|------||-------|------
         [      "R1",
                "",
-               "R1",
-               "R1",
+               "\\times 2/3 { d4(  <a' c>  e }  <g e'>4.)                c8(",
+               "a1",
         ], #1 ||-------|------||-------|------||-------|------||-------|------
         [      "R1",
                "",
-               "R1",
-               "R1",
+               "\\times 2/3 { d,4  <a' c>  e }   <g e'>2)                ",
+               "f'4              g4              a2",
         ], #2 ||-------|------||-------|------||-------|------||-------|------
         [      "R1",
                "",
-               "r2                              f4(             e",
-               "R1",
+               "f,8[(   d']     e[      c])      f4(             e",
+               "d4              c4              a2",
         ], #3 ||-------|------||-------|------||-------|------||-------|------
         [      "r2                              f4^\\p          e       ",
                "                                I               once    ",
                "c'2)                            f,8[(   d']     e[      c]",
-               "a'2                             d4(             <a c>~       ",
+               "a2                              d4(             <a c>~       ",
         ], #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
            #4 ||-------|------||-------|------||-------|------||-------|------
         [      "\\times 2/3 { f4   e      g }   g4              r8      f",
@@ -169,12 +173,12 @@ mouse_lines = [
                "\\times 2/3 { <c, a'>4(  g'2~ }  g4..)                    c,16",
         ], #6 ||-------|------||-------|------||-------|------||-------|------
         [      "r4              f^\\<           c               r8      c16  c",
-               "                when            fate                    in - ter ",
+               "                when            fate                    in-  ter- ",
                "<c, d f>2                       c8.      e16    c8      c16  c",
                "c4              c4              a4              bf4",
         ], #7 ||-------|------||-------|------||-------|------||-------|------
         [      "\\times 2/3 { g'4    g     g}   g4^\\!\\mf      r4",
-               "             -up  -ted    my    life ",
+               "             up-    ted    my    life ",
                "R1",
                "c2                              e,2             ",
         ],
@@ -207,17 +211,17 @@ swept_lines = [
         [      "r4              f               g4.                     f8",
                "                I               found                   my-",
                "R1",
-               "R1",
+               "r4              a4              bf2",
         ], #5 ||-------|------||-------|------||-------|------||-------|------
         [      "f4              r8      a8      \\times 2/3 {  a4     g    f }",
                "self                    in                    charge   of   a ",
-               "R1",
-               "R1",
+               "r2                              \\times 2/3 { <c f a>4     g    <c f> }",
+               "bf2                             <a f'>2",
         ], #6 ||-------|------||-------|------||-------|------||-------|------
         [      "f2.                                             r4",
                "play",
-               "R1",
-               "R1",
+               "<c d f>1",
+               "d1",
         ], #7 ||-------|------||-------|------||-------|------||-------|------
         [      "R1",
                "",
@@ -358,12 +362,12 @@ mouse2_lines = [
         copy.deepcopy(mouse_lines[5]),
            #6 ||-------|------||-------|------||-------|------||-------|------
         [      "r8      f^\\<   f       f       c       c       c4",
-               "        and     ne-     ver     thought I'd     be",
+               "        and     ne-     ver     thought I`d     be",
                "R1",# maybe this should be different to match the vocal line...
                copy.copy(mouse_lines[6][3]),
         ], #7 ||-------|------||-------|------||-------|------||-------|------
         [      copy.copy(mouse_lines[7][0]),
-               "             an- y- one's       wife",
+               "             an- y- one`s       wife",
                copy.copy(mouse_lines[7][2]), copy.copy(mouse_lines[7][3]),
         ],
 ]
@@ -477,7 +481,7 @@ tell_lines = [
         ],
     ]
 
-extend_lines(love_lines)
+#extend_lines(love_lines)
 
 extend_lines(mouse_lines)
 extend_lines(swept_lines)
@@ -491,9 +495,75 @@ extend_lines(choose_lines)
 
 extend_lines(tell_lines)
 
-voice_notes = love_arrangement.parts["voice"]
 
-love_arrangement.show_pdf()
+
+def full_output():
+    love_arrangement.make_score()
+    score = love_arrangement.score
+
+    spacing_vector = layouttools.make_spacing_vector(0, 0, 6, 0)
+    override(score).vertical_axis_group.staff_staff_spacing = spacing_vector
+    override(score).staff_grouper.staff_staff_spacing = spacing_vector
+    set_(score).mark_formatter = schemetools.Scheme('format-mark-box-numbers')
+    lilypond_file = lilypondfiletools.make_basic_lilypond_file(score)
+
+    # configure the lilypond file...
+    lilypond_file.global_staff_size = 16
+
+    context_block = lilypondfiletools.ContextBlock(
+        #source_context_name="Staff \RemoveEmptyStaves",
+        )
+    override(context_block).vertical_axis_group.remove_first = True
+    lilypond_file.layout_block.items.append(context_block)
+
+    # assume we can use default dimensions...
+
+    # bottom_margin = lilypondfiletools.LilyPondDimension(0.5, 'in')
+    # lilypond_file.paper_block.bottom_margin = bottom_margin
+
+    # top_margin = lilypondfiletools.LilyPondDimension(0.5, 'in')
+    # lilypond_file.paper_block.top_margin = top_margin
+
+    # left_margin = lilypondfiletools.LilyPondDimension(0.75, 'in')
+    # lilypond_file.paper_block.left_margin = left_margin
+
+    # right_margin = lilypondfiletools.LilyPondDimension(0.5, 'in')
+    # lilypond_file.paper_block.right_margin = right_margin
+
+    # paper_width = lilypondfiletools.LilyPondDimension(11, 'in')
+    # lilypond_file.paper_block.paper_width = paper_width
+
+    # paper_height = lilypondfiletools.LilyPondDimension(17, 'in')
+    # lilypond_file.paper_block.paper_height = paper_height
+
+    system_system_spacing = layouttools.make_spacing_vector(0, 0, 12, 0)
+    lilypond_file.paper_block.system_system_spacing = system_system_spacing
+
+    lilypond_file.header_block.composer = markuptools.Markup('Randall West')
+
+    # TO DO... move "for Taiko and Orchestra" to subtitle
+    lilypond_file.header_block.title = markuptools.Markup(love_arrangement.title)
+
+    #song_ly = '\\language "english" \n\r\n\r'
+    song_ly = ""
+    song_ly += "lovelyrics = \\lyricmode {" + lyrics + "} \n\r\n\r"
+    song_ly += format(lilypond_file)
+
+    song_ly = song_ly.replace('\\new Staff {', '\\new Staff {\n\r       \\new Voice ="voice" {', 1)
+
+    song_ly = song_ly.replace("\\new PianoStaff <<", "}\n\r    \\new Lyrics \\lyricsto voice \\lovelyrics \n\r    \\new PianoStaff <<")
+    
+    with open("love_song.ly", "w") as song_file:
+        song_file.write(song_ly)
+    
+    subprocess.call(["lilypond", "love_song.ly"])
+    systemtools.IOManager.open_file("love_song.pdf")
+
+    #get_pdf("love_song.ly")
+    
+full_output()
+
+#love_arrangement.show_pdf()
 
 # l_string = "\\addlyrics { yo la }"
 # v_string = "\\relative c' {a'1 | a1 }"
